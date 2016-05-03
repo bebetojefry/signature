aae0405f6227659a46cb0152c36c44006c217ca5
Bebeto Jefry <qbuser@qbadmins-Mac-Pro-38.local>
2016-05-03 18:24:21 +0530

    updated the rb script

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -15,13 +15,37 @@ set :dump_assetic_assets, true
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
-set   :use_sudo,      false
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", "vendor", "web/assets/vendor"]
 
-before 'symfony:assetic:dump', 'bower:install'
+set :writable_dirs,       ["app/cache", "app/logs"]
+set :webserver_user,      "www-data"
+set :permission_method,   :acl
+set :use_set_permissions, true
+
+set   :use_sudo,      true
+
+before 'symfony:assetic:dump', 'node:download'
+after 'node:download', 'bower:download'
+after 'bower:download', 'bower:install'
 after 'symfony:assetic:dump', 'symfony:permission'
 
+namespace :node do
+    desc 'Node download'
+    task :download do
+      capifony_pretty_print "--> Download NodeJS"
+      invoke_command "sh -c 'curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash - && sudo apt-get install -y nodejs'"
+      capifony_puts_ok
+    end
+end
+
 namespace :bower do
-    desc 'Run bower install'
+    desc 'Run bower'
+    task :download do
+      capifony_pretty_print "--> Downloading Bower"
+      invoke_command "sh -c 'sudo npm install -g bower'"
+      capifony_puts_ok
+    end
     task :install do
       capifony_pretty_print "--> Installing Bower dependencies"
       invoke_command "sh -c 'cd #{latest_release} && bower install'"
@@ -33,7 +57,7 @@ namespace :symfony do
     desc 'Setting permissions'
     task :permission do
       capifony_pretty_print "--> Setting permissions"
-      invoke_command "sh -c 'chmod -R 777 #{latest_release}/app/cache'"
+      invoke_command "sh -c 'chmod -R 777 #{latest_release}/app/cache && chmod -R 777 #{latest_release}/app/logs'"
       capifony_puts_ok
     end
 end
